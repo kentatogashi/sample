05e61058e35ba47a3bbb42b790412a959ddd9cc6
togashik <togashik@example.com>
2014-08-28 07:25:29 +0900

    implement registration of firm

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -3,5 +3,21 @@ class FirmsController < ApplicationController
         @firm = Firm.find(params[:id])
     end
     def new
+        @firm = Firm.new
+    end
+
+    def create 
+        @firm = Firm.new(firm_params)
+        if @firm.save
+            flash[:success] = "Welcome to the sample App!!"
+            redirect_to @firm
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def firm_params
+        params.require(:firm).permit(:name, :president, :phone,:email)
     end
 end
