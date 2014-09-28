b25d094ec431cd89169971b0c06398006568c1e4
kentatogashi <kenta_togasi@yahoo.co.jp>
2014-09-28 23:05:49 +0000

    Change the order of priority about card's url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get ':parse_string' => 'cards#index'
 
   get 'help' => 'help#index'
   get 'about' => 'about#index'
@@ -17,6 +16,7 @@ Rails.application.routes.draw do
   resources :firms do
       member { get :icon }
   end
+  get ':parse_string' => 'cards#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
