8afb9e7c81fd685ef19862949a328bbbb5cfda78
kentatogashi <kenta_togasi@yahoo.co.jp>
2014-10-04 10:28:40 +0000

    Take only log of cards in development

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,12 @@
 class CardsController < ApplicationController
   def index
     @card = Firm.where(:parse_string  => params[:parse_string])
-    logger = Logger.new("/home/vagrant/sample/log/development.log")
-    logger.debug(@card)
+
+    if Rails.env.developement?
+      logger = Logger.new("/home/vagrant/sample/log/development.log")
+      logger.debug(@card)
+    end
+    
     render 'index', :layout => false
   end
 
