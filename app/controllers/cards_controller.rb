4bb5d7555e146e2ff9bf6097c4898d4520a1a796
kentatogashi <kenta_togasi@yahoo.co.jp>
2014-10-05 02:06:41 +0000

    Redirect to the root path if card's url doesn't exist

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,9 +2,14 @@ class CardsController < ApplicationController
   def index
     @card = Firm.where(:parse_string  => params[:parse_string])
 
-    if Rails.env.developement?
+    if Rails.env.development?
       logger = Logger.new("/home/vagrant/sample/log/development.log")
       logger.debug(@card)
+    end
+
+    unless @card.exists?
+      redirect_to root_path
+      return
     end
     
     render 'index', :layout => false
